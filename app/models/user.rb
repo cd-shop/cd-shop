8b1f500f0f94fad772735b6d82d82ee5042b561b
19980410 <kkaoru1998@gmail.com>
2019-07-29 11:56:10 +0000

    ログイン画面の微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  	validates :tellnumber, presence: true, format: { with: VALID_PHONE_REGEX }
 end
