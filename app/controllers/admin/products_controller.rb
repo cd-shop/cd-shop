183b900f685784d528315f6bae80b97e7a2cfed0
19980410 <kkaoru1998@gmail.com>
2019-08-19 09:02:26 +0000

    conflict

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,15 +7,29 @@ class Admin::ProductsController < ApplicationController
         @artist = Artist.new
     end
     def create
-        product = Product.new(product_params)
-        genre = Genre.find_or_create_by(params[:id])
-        product.genre_id = genre.id
-        label = Label.find_or_create_by(params[:id])
-        product.label_id = label.id
-        artist = Artist.find_or_create_by(params[:id])
-        product.artist_id = artist.id
-        product.save
-        redirect_to admin_products_path
+        @genre = Genre.new(genre_params)
+        if @genre.save
+            redirect_to admin_products_path
+        end
+
+        @label = Label.new(label_params)
+        if @label.save
+            redirect_to admin_products_path
+        end
+        
+        @artist = Artist.new(artist_params)
+        if @artist.save
+            redirect_to admin_products_path
+        end
+        # product = Product.new(product_params)
+        # genre = Genre.find_or_create_by(genrename: params[:genrename])
+        # product.genre_id = genre.id
+        # label = Label.find_or_create_by(params[:id])
+        # product.label_id = label.id
+        # artist = Artist.find_or_create_by(params[:id])
+        # product.artist_id = artist.id
+        # product.save
+        # redirect_to admin_products_path
     end
 
     def index
@@ -36,22 +50,22 @@ class Admin::ProductsController < ApplicationController
 
     private
 
-    def product_params
-        params.require(:product).permit(:productname, :sale_status, :price, :image, genres_attributes: [:id, :genrename], artists_attributes: [:id, :artistname], labels_attributes: [:id, :labelname])
-    end
+    # def product_params
+    #     params.require(:product).permit(:productname, :sale_status, :price, :image, genres_attributes: [:genrename], artists_attributes: [:id, :artistname], labels_attributes: [:id, :labelname])
+    # end
         # def product_params
     #     params.require(:product).permit(:genre_id, :label_id, :artist_id, :productname, :sale_status, :price, :image)
     # end
 
-    # def genre_params
-    #     params.require(:genre).permit(:genrename)
-    # end
+    def genre_params
+        params.require(:genre).permit(:genrename)
+    end
 
-    # def label_params
-    #     params.require(:label).permit(:lablename)
-    # end
+    def label_params
+        params.require(:label).permit(:lablename)
+    end
 
-    # def artist_params
-    #     params.require(:artist).permit(:artistname)
-    # end
+    def artist_params
+        params.require(:artist).permit(:artistname)
+    end
 end
