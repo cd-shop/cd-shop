3f3afe850e5e89d6ec82868113294d4d84a25fb7
ashitani0803 <ashitani0803@gmail.com>
2019-08-21 05:44:45 +0000

    [create] ordersのコントローラ&index.html.erbの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'practices/index'
   root 'products#index'
   devise_for :users
-  resources :users
+  resources :users do
+  	resources :orders, only: [:index, :create]
+  end
   resources :products
 
   namespace :admin do
