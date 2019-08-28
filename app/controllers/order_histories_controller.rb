40673f842b74c1275c005517d02c374d95c9a8e5
19980410 <kkaoru1998@gmail.com>
2019-08-28 07:22:42 +0000

    購入履歴を少しできるよになった、保存はできる

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -1,12 +1,19 @@
 class OrderHistoriesController < ApplicationController
     def show
-
+        @order_history = OrderHistory.find(params[:id])
     end
 
     def create
         order_history = OrderHistory.new
-        order_history.order_id = Order.find(params[:order_id])
-        order_history.save!
+        order_history.user_id = current_user.id
+        order_history.order_id = params[:order_id]
+
+        # order_history.productname = current_user.cart_products.product.productname
+        money1 = order_history.order.subtotal
+        money2 = order_history.order.postage
+        order_history.total_amount = money1 + money2
+        order_history.save
         redirect_to user_order_history_path(current_user.id, order_history.id)
     end
 end
+
--- app/models/order_history.rb
+++ app/models/order_history.rb
@@ -1,3 +1,4 @@
 class OrderHistory < ApplicationRecord
     belongs_to :order
+	enum shipment_status: {発送準備中: 0, 発送済: 1, 到着済み: 2}
 end
