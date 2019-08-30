ef3e9fec88a4b6ac4bf708343bc7ba960377890d
19980410 <kkaoru1998@gmail.com>
2019-08-30 06:20:21 +0000

    品切れと販売中の区別、バリデーション

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -14,7 +14,6 @@ class CartProductsController < ApplicationController
 
     def destroy
         cart_product = CartProduct.find_by(product_id: params[:product_id])
-        binding.pry
         cart_product.destroy
         redirect_to cart_products_path
     end
--- app/models/cd.rb
+++ app/models/cd.rb
@@ -3,6 +3,8 @@ class Cd < ApplicationRecord
 
 	has_many :songs, dependent: :destroy
 
+	validates :cdname, presence: true
+
 	accepts_nested_attributes_for :songs, allow_destroy: true
 
 end
